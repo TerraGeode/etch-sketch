e28ff1d0f35595472387c2226fccc4bb355272e1
TerraGeode <77182889+TerraGeode@users.noreply.github.com>
2024-09-05 14:04:07 -0400

    Fixed problem with grid leaving unnecessary spaces

--- etching.js
+++ etching.js
@@ -1,12 +1,18 @@
 const pad = document.querySelector("#pad");
 
-for (let i = 0; i <16; i +=1) {
+let pixelCount = 16;
+let pixelSIze = Math.floor(500/pixelCount)
+
+for (let i = 0; i <pixelCount; i +=1) {
     const pRow = document.createElement("div");
     console.log(i);
     pRow.style.display = "flex";
-    for (let y = 0; y < 16; y +=1) {
+    pRow.style.flex = "1";
+    pRow.style.flexDirection = "row";
+    pRow.style.height = pixelSIze + "px";
+    for (let y = 0; y < pixelCount; y +=1) {
         const pixel = document.createElement("div");
-        pixel.style.padding = "5px"
+        pixel.style.width = pixelSIze + "px";
         pRow.appendChild(pixel);
     }
     pad.appendChild(pRow);
