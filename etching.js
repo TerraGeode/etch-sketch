6f545492e26a779f7712e47e3bc41928f791154e
TerraGeode <77182889+TerraGeode@users.noreply.github.com>
2024-09-05 15:56:06 -0400

    Added function to remove old grid and generate new grid.

--- etching.js
+++ etching.js
@@ -1,7 +1,7 @@
 const pad = document.querySelector("#pad");
 
 let pixelCount = 16;
-let pixelSIze = Math.floor(500/pixelCount)
+let pixelSIze = Math.floor(500/pixelCount);
 
 for (let i = 0; i <pixelCount; i +=1) {
     const pRow = document.createElement("div");
@@ -21,3 +21,40 @@ for (let i = 0; i <pixelCount; i +=1) {
     }
     pad.appendChild(pRow);
 }
+
+let newGrid = () => {
+    pixelCount = parseInt(prompt("Enter a value between 10 and 100"));
+    while (Number.isInteger(pixelCount) && pixelCount >= 100 && pixelCount <= 10) {
+        pixelCount = parseInt(prompt("Enter a value between 10 and 100"));
+    }
+    pixelSIze = Math.floor(500/pixelCount);
+    for (let i = 0; i <pixelCount; i +=1) {
+        const pRow = document.createElement("div");
+        console.log(i);
+        pRow.style.display = "flex";
+        pRow.style.flex = "1";
+        pRow.style.flexDirection = "row";
+        for (let y = 0; y < pixelCount; y +=1) {
+            const pixel = document.createElement("div");
+            pixel.style.width = pixelSIze + "px";
+            pixel.style.height = pixelSIze + "px";
+            pixel.style.border = "solid 1px black";
+            pixel.addEventListener("mouseover", () => {
+                pixel.style.backgroundColor = "grey";
+            });
+            pRow.appendChild(pixel);
+        }
+        pad.appendChild(pRow);
+    }
+}
+
+const btn = document.querySelector("#fun");
+btn.addEventListener("click", () => {
+    while (pad.firstChild) {
+        while (pad.firstChild.firstChild) {
+            pad.firstChild.firstChild.remove();
+        }
+        pad.firstChild.remove();
+    }
+    newGrid();
+});
