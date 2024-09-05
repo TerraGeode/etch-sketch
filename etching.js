ab2320294032143755f34d52932c3ed6760255ce
TerraGeode <77182889+TerraGeode@users.noreply.github.com>
2024-09-05 14:13:46 -0400

    Made pixels change color when mouse hovers over them.

--- etching.js
+++ etching.js
@@ -9,10 +9,14 @@ for (let i = 0; i <pixelCount; i +=1) {
     pRow.style.display = "flex";
     pRow.style.flex = "1";
     pRow.style.flexDirection = "row";
-    pRow.style.height = pixelSIze + "px";
     for (let y = 0; y < pixelCount; y +=1) {
         const pixel = document.createElement("div");
         pixel.style.width = pixelSIze + "px";
+        pixel.style.height = pixelSIze + "px";
+        pixel.style.border = "solid 1px black";
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "grey";
+        });
         pRow.appendChild(pixel);
     }
     pad.appendChild(pRow);
